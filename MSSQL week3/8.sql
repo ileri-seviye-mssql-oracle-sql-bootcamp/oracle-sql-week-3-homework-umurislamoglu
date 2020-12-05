a31749831d4117ad93eb3e9a42de2400894c2503
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2020-12-05 18:43:01 +0300

    Delete 8.sql

--- MSSQL week3/8.sql
+++
@@ -1,5 +0,0 @@
-select concat(k.isim,' ',k.soyad) , count(s.faturaKod) as siparisadet from tblkullanici k
-left join tblsiparis s on s.kullaniciKod =k.kullaniciKod
-where k.isim like 'A%' and k.soyad like '%ak%'
-group by concat(k.isim,' ',k.soyad)
-order by siparisadet desc
