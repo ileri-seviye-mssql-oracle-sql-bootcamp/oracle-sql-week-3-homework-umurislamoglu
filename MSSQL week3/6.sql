1857919765812d28095899c0a3c7ee0678cf7230
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2020-12-05 18:42:50 +0300

    Delete 6.sql

--- MSSQL week3/6.sql
+++
@@ -1,5 +0,0 @@
-select count(s.faturaKod) as siparisadet, m.meslek from tblSiparis s
-left join tblKullanici k on s.kullaniciKod =k.kullaniciKod
-left join tblMeslek m on k.meslekKod =m.meslekKod
-group by m.meslek
-order by siparisadet desc
